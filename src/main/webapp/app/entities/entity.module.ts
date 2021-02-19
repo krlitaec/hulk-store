6bebfc0f523edb16e6428e9e4efe80556dff9a5f
krlitaec23 <kaguirre@mail.com>
2021-02-19 17:20:33 -0500

    Creación de entidades: Category, Product y Kardex

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'category',
+        loadChildren: () => import('./category/category.module').then(m => m.HulkStoreCategoryModule),
+      },
+      {
+        path: 'product',
+        loadChildren: () => import('./product/product.module').then(m => m.HulkStoreProductModule),
+      },
+      {
+        path: 'kardex',
+        loadChildren: () => import('./kardex/kardex.module').then(m => m.HulkStoreKardexModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
